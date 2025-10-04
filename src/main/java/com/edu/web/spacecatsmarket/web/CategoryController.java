9beaf30b4cca248db77f4bb7503e30c023b9a50b
MartyniukYurii <145149346+masonabor@users.noreply.github.com>
2025-10-04 18:32:47 +0300

    adding Validation annotation to controller

--- src/main/java/com/edu/web/spacecatsmarket/web/CategoryController.java
+++ src/main/java/com/edu/web/spacecatsmarket/web/CategoryController.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @Controller
+@Validated
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/categories")
 public class CategoryController {
