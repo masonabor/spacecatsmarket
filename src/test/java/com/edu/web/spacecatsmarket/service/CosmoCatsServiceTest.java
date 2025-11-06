8382b102cdd4e93fec444e3ec67d951f47a25f30
MartyniukYurii <145149346+masonabor@users.noreply.github.com>
2025-11-06 20:20:45 +0200

    lab2 fixing CosmoCatsServiceTest

--- src/test/java/com/edu/web/spacecatsmarket/service/CosmoCatsServiceTest.java
+++ src/test/java/com/edu/web/spacecatsmarket/service/CosmoCatsServiceTest.java
@@ -1,21 +1,25 @@
 package com.edu.web.spacecatsmarket.service;
 
+import com.edu.web.spacecatsmarket.featuretoggle.FeatureToggleService;
 import com.edu.web.spacecatsmarket.featuretoggle.FeatureToggles;
+import com.edu.web.spacecatsmarket.featuretoggle.aspect.CosmoCatsAspect;
 import com.edu.web.spacecatsmarket.featuretoggle.exception.FeatureCosmoCatsNotEnabledException;
-import com.edu.web.spacecatsmarket.featuretoggle.impl.FeatureToggleServiceImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest(classes = {CosmoCatsService.class, FeatureToggleServiceImpl.class})
+@SpringBootTest(classes = {CosmoCatsService.class, CosmoCatsAspect.class})
+@EnableAspectJAutoProxy
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @DisplayName("Cosmo cats service Tests")
 public class CosmoCatsServiceTest {
@@ -24,12 +28,13 @@ public class CosmoCatsServiceTest {
     private CosmoCatsService cosmoCatsService;
 
     @MockitoBean
-    private FeatureToggleServiceImpl featureToggleService;
+    private FeatureToggleService featureToggleService;
 
     @Test
     @DisplayName("Should return list of cats when cosmo-cats feature is enabled")
     void shouldReturnCosmoCatsWhenFeatureEnabled() {
-        featureToggleService.enable(FeatureToggles.COSMO_CATS);
+
+        when(featureToggleService.check(FeatureToggles.COSMO_CATS)).thenReturn(true);
 
         var cats = cosmoCatsService.getCosmoCats();
 
@@ -40,7 +45,8 @@ public class CosmoCatsServiceTest {
     @Test
     @DisplayName("Should throw FeatureCosmoCatsNotEnableException when feature cosmo-cats is disabled")
     void shouldThrowFeatureCosmoCatsNotEnableExceptionWhenFeatureDisabled() {
-        featureToggleService.disable(FeatureToggles.COSMO_CATS);
+
+        when(featureToggleService.check(FeatureToggles.COSMO_CATS)).thenReturn(false);
 
         assertThrows(FeatureCosmoCatsNotEnabledException.class, () -> cosmoCatsService.getCosmoCats());
     }
